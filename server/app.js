9b505577197b8e9d342fcf987f44875671fca1fb
inFngNam <inf.ng.nam@gmail.com>
2020-03-28 10:41:53 +0700

    first setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const mongoose = require("mongoose");
+const port = 3001;
+const MONGO_URI = 'mongodb+srv://admin:admin@dragonlearncluster-9aoa7.mongodb.net/test?retryWrites=true&w=majority';
 
-app.get('/', (req, res) => res.send('Hello LKKK!'))
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.once('open', function () {
+  console.log('connected mongodb');
+}); 
+
+app.get("/", (req, res) => res.send("Dragon Learn"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
