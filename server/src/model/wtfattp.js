1d300726ab1bcfcdee24b57945ed66af8ba9b0cc
inFngNam <inf.ng.nam@gmail.com>
2020-04-27 16:10:08 +0700

    wtfattp game

--- server/src/model/wtfattp.js
+++ server/src/model/wtfattp.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var wtfattp_data = new Schema ({
+var wtfattp_data = new Schema({
     path: { type: String, required: true },
     ts: { type: Number, required: true },
     ms: { type: Number, required: true }
 });
 
-
+wtfattp_data.index({ ts: 1, ms: 1, path: 1 }, { unique: true });
 var WtfattpData = mongoose.model('WtfattpData', wtfattp_data);
 module.exports = WtfattpData; 
