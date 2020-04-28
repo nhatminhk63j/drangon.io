df1cbb56c1bcf479d35eb36c6ae6eefb80240b04
Nhat Minh <nhatngo11a1@gmail.com>
2020-04-28 08:43:10 +0700

    Updates

---
+++ precache-manifest.9f5df9d22846f53125e94d003c2258e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03466e001815d176deffcd6ea8167fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e150689e3c8603c07b0",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "57eaeec2407113805b9c",
+    "url": "/static/css/main.5662e61e.chunk.css"
+  },
+  {
+    "revision": "5e150689e3c8603c07b0",
+    "url": "/static/js/2.0dda80d8.chunk.js"
+  },
+  {
+    "revision": "6748d75251e997c215feee7dc9bf67ed",
+    "url": "/static/js/2.0dda80d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57eaeec2407113805b9c",
+    "url": "/static/js/main.1e34ebeb.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
