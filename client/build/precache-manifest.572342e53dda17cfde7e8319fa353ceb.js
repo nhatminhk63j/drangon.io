8c0938a343a59cddcf8e01cb47031e75a866bfd5
Nhat Minh <nhatngo11a1@gmail.com>
2020-04-28 09:05:51 +0700

    build projec

---
+++ client/build/precache-manifest.572342e53dda17cfde7e8319fa353ceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbe9a56ba8e1adf6408d71469367e39",
+    "url": "/drangonlearn.io/index.html"
+  },
+  {
+    "revision": "5e150689e3c8603c07b0",
+    "url": "/drangonlearn.io/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "500faba3ef93cc751fb5",
+    "url": "/drangonlearn.io/static/css/main.5662e61e.chunk.css"
+  },
+  {
+    "revision": "5e150689e3c8603c07b0",
+    "url": "/drangonlearn.io/static/js/2.0dda80d8.chunk.js"
+  },
+  {
+    "revision": "6748d75251e997c215feee7dc9bf67ed",
+    "url": "/drangonlearn.io/static/js/2.0dda80d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500faba3ef93cc751fb5",
+    "url": "/drangonlearn.io/static/js/main.7bbc1990.chunk.js"
+  },
+  {
+    "revision": "7114d4ee4c1ad047873b",
+    "url": "/drangonlearn.io/static/js/runtime-main.2f484232.js"
+  }
+]);
